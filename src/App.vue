df26da127ccdfb6e0fab718f37014bb74d349fac
Azhari Subroto <juarathemes@gmail.com>
2020-03-05 18:58:04 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus">
+body
+  background: #FFFAE6
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="homepage">
+    TEST
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
