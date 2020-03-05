291abcbb114f691c8333462b705fabe0a9560d36
Azhari Subroto <juarathemes@gmail.com>
2020-03-05 21:47:00 +0700

    base setup

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import cssreset from './assets/styles/reset.css'
 
+Vue.use(cssreset)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
